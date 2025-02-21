08d08cf96c1d69ba3aaba4d5c975c71507f299d4
Jacob Chang <jacobchang@Jacobs-MacBook-Pro-2.local>
2025-02-20 16:31:36 -0800

    Add exports

--- R/spomic.R
+++ R/spomic.R
@@ -1,6 +1,7 @@
 install.packages("spatstat.geom")
 library(spatstat.geom)
 
+#' @export
 setClass(
   "Spomic",
   slots = c(
@@ -15,7 +16,7 @@ setClass(
   )
 )
 
-
+#' @export
 validate_columns <- function(df, required_columns) {
   missing_columns <- setdiff(required_columns, colnames(df))
   if (length(missing_columns) > 0) {
@@ -23,16 +24,18 @@ validate_columns <- function(df, required_columns) {
   }
 }
 
+#' @export
 spomic_to_pp <- function(df) {
   xrange <- range(df$x)
   yrange <- range(df$y)
-  pp <- spatstat.geom::ppp(df$x, 
+  pp <- spatstat.geom::ppp(df$x,
             df$y,
-            window = spatstat.geom::owin(xrange, yrange), 
+            window = spatstat.geom::owin(xrange, yrange),
             marks = factor(df$cell_type))
   return(pp)
 }
 
+#' @export
 createSpomic <- function(p, drop_na = TRUE) {
   # Check input type and read data
   if (is.character(p)) {
@@ -43,37 +46,37 @@ createSpomic <- function(p, drop_na = TRUE) {
   } else {
     stop("Input must be a data.frame or path to a CSV file.")
   }
-  
+
   if (drop_na) {
     df <- tidyr::drop_na(df)
   }
-  
+
   if (nrow(df) == 0) stop("Data frame is empty.")
-  
+
   # Validate required columns
   required_columns <- c("x", "y", "cell_type", "sample")
   validate_columns(df, required_columns)
-  
+
   # Ensure correct column types
   if (!is.numeric(df$x) || !is.numeric(df$y)) {
     stop("Columns 'x' and 'y' must contain numeric values.")
   }
-  
+
   # Ensure `sample` column has a single unique value
   unique_samples <- unique(df$sample)
   if (length(unique_samples) > 1) {
     stop("Data frame contains multiple unique sample IDs. Ensure only one sample per Spomic object.")
   }
-  
+
   # Create Spomic object
   object <- new("Spomic",
                 sample = as.character(unique_samples),
                 df = df,
-                pp = spomic_to_pp(df), 
+                pp = spomic_to_pp(df),
                 null_envelope = data.frame(),
                 hyperparameters = list()
   )
-  
+
   return(object)
 }
 
