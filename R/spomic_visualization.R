9e4f299326ea408a18ce48a2bfad5b13b7d3ad8e
Jacob Chang <jacobchang@Jacobs-MacBook-Pro-2.local>
2025-03-03 18:09:40 -0800

    Update plotting functions to align with CELESTA plots

--- R/spomic_visualization.R
+++ R/spomic_visualization.R
@@ -1,8 +1,27 @@
 usethis::use_package("ggplot2")
 
+#' @export
+plot_spomic <- function(spomic, point_size = 0.5, scale_dot_size = 2) {
+  g <- ggplot(spomic@df, aes(x=x_rescaled, y=y_rescaled, group=cell_type)) +
+    geom_point(aes(color = cell_type), size=point_size) +
+    labs(title=spomic@details$sample) +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.title = element_blank(),
+      legend.text = element_text(size=8),
+      panel.background = element_rect(fill = 'black'),
+      axis.line = element_line(colour = "black"),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank()
+    ) +
+    guides(colour = guide_legend(override.aes = list(size=2))) +
+    coord_fixed(ratio = 1)
+  return(g)
+}
 
 #' @export
-plotCellProportions <- function(spomic, stack = TRUE) {
+plot_cell_proportions <- function(spomic, stack = TRUE) {
   proportions <- spomic@df |>
     dplyr::group_by(cell_type) |>
     dplyr::summarise(count = dplyr::n()) |>
@@ -31,56 +50,10 @@ plotCellProportions <- function(spomic, stack = TRUE) {
   return(p)
 }
 
-
-#' #' @export
-#' plotSpomic <- function(spomic_obj, point_size = 0.75){
-#'   g <- ggplot(spomic_obj@df, aes(x = x, y = y, color = cell_type)) +
-#'     geom_point(size = point_size) +
-#'     coord_fixed(ratio = 1) +
-#'     theme_minimal() +
-#'     theme(plot.background = element_rect(fill = "black"),
-#'           panel.grid.major = element_blank(),
-#'           panel.grid.minor = element_blank(),
-#'           axis.text = element_blank(),
-#'           legend.title = element_blank(),
-#'           legend.position = "bottom",
-#'           legend.text = element_text(color = "white"),
-#'           legend.spacing.y = unit(0.1, 'cm'),    # Reduce vertical spacing
-#'           legend.key.size = unit(0.5, 'cm'),       # Increase color legend size
-#'           legend.background = element_rect(fill = "black", color = NA),  # Black legend background
-#'           legend.box.background = element_rect(fill = "black", color = NA)  # Black legend box background
-#'     ) +
-#'     guides(color = guide_legend(override.aes = list(size = 2.5)))  # Control color legend point size
-#'
-#'   return(g)
-#' }
 #' @export
-plot_spomic <- function(spomic, point_size = 0.75){
-  g <- ggplot(spomic@df, aes(x = x, y = y, color = cell_type)) +
-    geom_point(size = point_size) +
-    coord_fixed(ratio = 1) +
-    theme_minimal() +
-    theme(plot.background = element_rect(fill = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.text = element_blank(),
-          legend.title = element_blank(),
-          legend.position = "bottom",
-          legend.text = element_text(color = "white"),
-          legend.spacing.y = unit(0.1, 'cm'),    # Reduce vertical spacing
-          legend.key.size = unit(0.5, 'cm'),       # Increase color legend size
-          legend.background = element_rect(fill = "black", color = NA),  # Black legend background
-          legend.box.background = element_rect(fill = "black", color = NA)  # Black legend box background
-    ) +
-    guides(color = guide_legend(override.aes = list(size = 2.5)))  # Control color legend point size
-
-  return(g)
-}
-
-#' @export
-plotCellPair <- function(spomic_obj, cellA, cellB, point_size = 0.75){
+plot_cell_pair <- function(spomic_obj, i, j, point_size = 0.75){
   df <- spomic_obj@df |>
-    dplyr::mutate(cells = dplyr::case_when(cell_type == !!cellA | cell_type == !!cellB ~ as.character(cell_type),
+    dplyr::mutate(cells = dplyr::case_when(cell_type == !!i | cell_type == !!j ~ as.character(cell_type),
                                            TRUE ~ "Other"))
 
   unique_cells <- unique(df$cells)
@@ -88,28 +61,29 @@ plotCellPair <- function(spomic_obj, cellA, cellB, point_size = 0.75){
   other_color <- "gray50"
 
   # Constructing color mapping dynamically
-  color_values <- c(setNames(default_colors, c(cellA, cellB)), Other = other_color)
+  color_values <- c(setNames(default_colors, c(i, j)), Other = other_color)
 
   g <- ggplot(df, aes(x = x, y = y, color = cells)) +
     geom_point(size = point_size) +
     coord_fixed(ratio = 1) +
     theme_minimal() +
     scale_color_manual(values = color_values) +
-    theme(plot.background = element_rect(fill = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.text = element_blank(),
-          legend.title = element_blank(),
-          legend.position = "bottom",
-          legend.text = element_text(color = "white"),
-          legend.spacing.y = unit(0.1, 'cm'),    # Reduce vertical spacing
-          legend.key.size = unit(0.5, 'cm'),       # Increase color legend size
-          legend.background = element_rect(fill = "black", color = NA),  # Black legend background
-          legend.box.background = element_rect(fill = "black", color = NA)  # Black legend box background
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.title = element_blank(),
+      legend.text = element_text(size=8),
+      panel.background = element_rect(fill = 'black'),
+      axis.line = element_line(colour = "black"),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank()
     ) +
-    guides(color = guide_legend(override.aes = list(size = 2.5)))
+    guides(colour = guide_legend(override.aes = list(size=2))) +
+    coord_fixed(ratio = 1)
+
   return(g)
 }
+
 
 #' @export
 plotKDE <- function(spomic, cell) {
@@ -132,9 +106,22 @@ plotKDE <- function(spomic, cell) {
     geom_polygon(data = hull_data, aes(x, y), fill = NA, color = "white", linewidth = 1) +
 
     # Title & Theme
-    ggtitle(paste(cell, "Kernel Density with Convex Hull Outline")) +
-    ggpubr::theme_pubclean() +
-    labs(fill = "Kernel Density")
-
+    ggtitle(paste("Kernel Density of", cell)) +
+    # ggpubr::theme_pubclean() +
+    labs(fill = "Kernel Density") +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.title = element_blank(),
+      legend.text = element_text(size=8),
+      panel.background = element_rect(fill = 'black'),
+      axis.line = element_line(colour = "black"),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank()
+    ) +
+    guides(colour = guide_legend(override.aes = list(size=2))) +
+    coord_fixed(ratio = 1)
+  p
   return(p)
 }
+
