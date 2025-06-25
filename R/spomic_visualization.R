f42985d65c33223f908322a54da89f68cd3fa524
Jacob Chang <jacobchang@Mac.attlocal.net>
2025-06-24 19:10:04 -0700

    Update gitignore

--- R/spomic_visualization.R
+++ R/spomic_visualization.R
@@ -1,11 +1,12 @@
 usethis::use_package("ggplot2")
 
-
+#' @export
 spomic_style <- function(x) {
   x |>
     tidyplots::adjust_font(family = "sans")
 }
 
+#' @export
 plot_spomic <- function(spomic, point_size = 1) {
   spomic@df |>
     tidyplots::tidyplot(x = x, y = y, color = cell_type) |>
@@ -17,6 +18,7 @@ plot_spomic <- function(spomic, point_size = 1) {
     spomic_style()
 }
 
+#' @export
 plot_cell_proportions <- function(spomic) {
   N <- nrow(spomic@df)
   spomic@df |>
@@ -34,62 +36,10 @@ plot_cell_proportions <- function(spomic) {
     tidyplots::adjust_legend_position("right") |>
     spomic_style()
 }
-#'
-#' #' @export
-#' plot_spomic <- function(spomic, point_size = 0.5, scale_dot_size = 2) {
-#'   spomic@df$x_rescaled <- spomic@df$x - min(spomic@df$x)
-#'   spomic@df$y_rescaled <- spomic@df$y - min(spomic@df$y)
-#'
-#'   g <- ggplot(spomic@df, aes(x=x_rescaled, y=y_rescaled, group=cell_type)) +
-#'     geom_point(aes(color = cell_type), size=point_size) +
-#'     labs(title=spomic@details$sample) +
-#'     theme(
-#'       panel.grid.major = element_blank(),
-#'       panel.grid.minor = element_blank(),
-#'       legend.title = element_blank(),
-#'       legend.text = element_text(size=8),
-#'       panel.background = element_rect(fill = 'black'),
-#'       axis.line = element_line(colour = "black"),
-#'       axis.title.x = element_blank(),
-#'       axis.title.y = element_blank()
-#'     ) +
-#'     guides(colour = guide_legend(override.aes = list(size=2))) +
-#'     coord_fixed(ratio = 1)
-#'   return(g)
-#' }
-#'
-#' #' @export
-#' plot_cell_proportions <- function(spomic, stack = TRUE) {
-#'   proportions <- spomic@df |>
-#'     dplyr::group_by(cell_type) |>
-#'     dplyr::summarise(count = dplyr::n()) |>
-#'     dplyr::mutate(proportion = count / sum(count),
-#'                   label = paste0(round(proportion * 100, 1), "%"),
-#'                   cell_type = forcats::fct_reorder(cell_type, proportion, .desc = TRUE))
-#'
-#'   if(stack) {
-#'     p <- ggplot(proportions, aes(x = "", y = proportion, fill = cell_type)) +
-#'       geom_bar(stat = "identity", width = 0.25) +
-#'       scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-#'       labs(title = "Proportions of Cell Types", x = NULL, y = "Proportion") +
-#'       ggpubr::theme_pubclean() +
-#'       theme(axis.text.x = element_blank(),  # Remove x-axis text
-#'             axis.ticks.x = element_blank())
-#'   }
-#'
-#'   if(!stack) {
-#'     p <- ggplot(proportions, aes(x = reorder(cell_type, proportion), y = proportion)) +
-#'       geom_bar(stat = "identity") +
-#'       coord_flip() +
-#'       labs(x = "Cell Type", y = "Proportion", title = "Cell Type Proportion") +
-#'       theme_minimal()
-#'   }
-#'
-#'   return(p)
-#' }
-#'
 
 
+
+#' @export
   plot_cell_pair <- function(spomic, i, j) {
     three_colors <- tidyplots::new_color_scheme(x=c("#d7d2cb", "#0371b2", "#d36027"))
     count_i <- sum(spomic@df$cell_type == i)
@@ -125,39 +75,6 @@ plot_cell_proportions <- function(spomic) {
 
 
 
-#' @export
-# plot_cell_pair <- function(spomic_obj, i, j, point_size = 0.75){
-#   df <- spomic_obj@df |>
-#     dplyr::mutate(cells = dplyr::case_when(cell_type == !!i | cell_type == !!j ~ as.character(cell_type),
-#                                            TRUE ~ "Other"))
-#
-#   unique_cells <- unique(df$cells)
-#   default_colors <- c("red", "blue")  # Default colors for cellA and cellB
-#   other_color <- "gray50"
-#
-#   # Constructing color mapping dynamically
-#   color_values <- c(setNames(default_colors, c(i, j)), Other = other_color)
-#
-#   g <- ggplot(df, aes(x = x, y = y, color = cells)) +
-#     geom_point(size = point_size) +
-#     coord_fixed(ratio = 1) +
-#     theme_minimal() +
-#     scale_color_manual(values = color_values) +
-#     theme(
-#       panel.grid.major = element_blank(),
-#       panel.grid.minor = element_blank(),
-#       legend.title = element_blank(),
-#       legend.text = element_text(size=8),
-#       panel.background = element_rect(fill = 'black'),
-#       axis.line = element_line(colour = "black"),
-#       axis.title.x = element_blank(),
-#       axis.title.y = element_blank()
-#     ) +
-#     guides(colour = guide_legend(override.aes = list(size=2))) +
-#     coord_fixed(ratio = 1)
-#
-#   return(g)
-# }
 
 
 #' @export
